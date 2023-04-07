9ddc430b1814454df0ca45450099622d83e99fc6
Szulyka <palko@horogszegi.hu>
2023-04-07 17:49:28 +0200

    files

---
+++ elmeny.js
@@ -0,0 +1,9 @@
+function nagyBetu() {
+    let theme = document.getElementById('alap_stilus');
+    theme.href = "nagybetu.css";
+}
+
+function Alap() {
+    let theme = document.getElementById('alap_stilus');
+    theme.href = "stilus.css";
+}
